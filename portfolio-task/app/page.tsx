842ed300951f1354719af9684ecf7f18d07c86b4
yasin.jsdev <yasin.jsdev@gmail.com>
2026-05-26 15:18:36 +0530

    taks one

--- portfolio-task/app/page.tsx
+++ portfolio-task/app/page.tsx
@@ -2,6 +2,8 @@ import Hero from "@/components/Hero/Hero";
 import PostsContainer from "@/components/Posts/PostsContainer";
 import FeaturedWork from "@/components/Featured/FeaturedWork";
 
+// taks one
+
 export default function Home() {
   return (
     <main className="main">
